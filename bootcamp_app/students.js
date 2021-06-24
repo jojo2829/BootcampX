f7e0f7d35708dba8278dfa70121d57ccf512fd02
jojo2829 <joulinli@gmail.com>
2021-06-24 04:08:09 +0000

    Parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,26 +7,32 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+// pool.query(`
+// SELECT students.id AS id, students.name AS student, cohorts.name AS cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
+const queryString = `
 SELECT students.id AS id, students.name AS student, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-LIMIT 5;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-  })
-})
-.catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
-pool.query(`
-SELECT students.id AS id, students.name AS student, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
+const cohort = process.argv[2];
+const limit = process.argv[3];
+
+const values = [`%${cohort}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
